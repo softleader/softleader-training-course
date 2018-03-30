e7935c8ab0e5e3773b4adc04d26b71df5e84e3cf
sevenlfanks <godjdc@gmail.com>
2018-03-30 12:18:54 +0800

    update

--- 2018Q1/stream-adv/src/main/java/tw/com/softleader/practice/answer/CountTxtsByYearMonthAnswerApp.java
+++ 2018Q1/stream-adv/src/main/java/tw/com/softleader/practice/answer/CountTxtsByYearMonthAnswerApp.java
@@ -1,25 +1,18 @@
 package tw.com.softleader.practice.answer;
 
 import java.io.File;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.YearMonth;
-import java.time.chrono.MinguoDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.google.common.collect.Lists;
-
-import tw.com.softleader.commons.function.Parses;
 import tw.com.softleader.sample.MergeTxtsToZipSampleApp;
 
 public class CountTxtsByYearMonthAnswerApp {
@@ -50,49 +43,6 @@ public class CountTxtsByYearMonthAnswerApp {
 				.collect(Collectors.groupingBy(ci -> YearMonth.from(ci.getDate())))
 				.forEach((ym, caseInfos) -> System.out.println(ym + ": " + caseInfos.size()));
 
-	}
-
-	/**
-	 * 不使用Stream的範例
-	 * @throws Exception
-	 */
-	public static void nonStream() throws Exception {
-		final URL txtsUrl = CountTxtsByYearMonthAnswerApp.class.getResource("/txts");
-		final Path txtPath = Paths.get(txtsUrl.toURI());
-
-		final Map<YearMonth, List<String>> result = new TreeMap<>();
-		final File[] files = txtPath.toFile().listFiles();
-		for (File file : files) {
-			// txts下第一層一定是資料夾，不是資料夾的就略過
-			if (file.isDirectory()) {
-				// 資料夾的名稱即為案件編號
-				final String caseNo = file.getName();
-				final String dateStr;
-				if (caseNo.startsWith("HW")) {
-					dateStr = caseNo.substring(3, 10);
-				} else if (caseNo.startsWith("TP")) {
-					dateStr = "1" + caseNo.substring(2, 8);
-				} else {
-					// 非預期的開頭不處理
-					continue;
-				}
-
-				// 處理民國年字串轉日期
-				final MinguoDate date = Parses.tryMinguoDate(dateStr, FORMATTER);
-				// 擷取年月資訊
-				final YearMonth yearMonth = YearMonth.from(date);
-
-				// 蒐集
-				if (!result.containsKey(yearMonth)) {
-					result.put(yearMonth, Lists.newArrayList());
-				}
-				result.get(yearMonth).add(caseNo);
-			}
-		}
-
-		result.forEach((ym, caseNos) -> {
-			System.out.println(ym + ": " + caseNos.size());
-		});
 	}
 
 }
