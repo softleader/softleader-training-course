e151386d6e365fca5c8bd66ae75b2b5991943591
Matt S.Y. Ho <methodho@gmail.com>
2018-08-21 11:30:57 +0800

    update

--- 2016Q3/react-workshop/react/js/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import PolForm from './components/PolForm.jsx';
-
-ReactDOM.render(
-  <PolForm />,
-  document.getElementById('myForm')
-);
--- 2016Q3/react-workshop/react/js/constants/Events.jsx
+++
@@ -1,7 +0,0 @@
-export const CommonEvents = {
-	DATA_CHANGE: "DATA_CHANGE"
-};
-
-export const PolEvents = {
-	POL_SUBMIT: "POL_SUBMIT"
-};
